fe44ec46eed43f07e13f6c4caa1575f80c1147ab
BraggTroy <bragg_chen@163.com>
2018-10-11 11:05:22 +0800

    。。。

---
+++ assets/js/prototype-test.js
@@ -0,0 +1,33 @@
+
+
+
+function Person() {
+}
+Person.prototype = {
+    name: 'jiang',
+    job: 'student',
+    sayName: function() {
+        console.log(this.name)
+    }
+};
+
+var person1 = new Person();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
